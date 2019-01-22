44354cbbf6999165c92dddd84e98b5c0a2bb476a
rickyjian <rjricky03bc@gmail.com>
2019-01-23 01:16:24 +0800

    fetch inner func to outer

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/RickyJian/template"
 	"github.com/gin-gonic/gin"
 	"library/controller"
@@ -12,22 +11,15 @@ var b *controller.Book
 
 func initServer() {
 	router := gin.Default()
-	router.Static("/assets/","assets/static/")
-	router.StaticFile("/favicon.ico", "assets/favicon.ico")
 	t := template.TemplateWalk("assets/template/",".html")
 	router.SetHTMLTemplate(t)
-	router.GET("/", func(c *gin.Context) {
-		fmt.Println("test")
-		c.HTML(http.StatusOK, "index", gin.H{
-			"title": "首頁",
-		})
-	})
-	router.GET("/inner", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "inner/index", gin.H{
-			"title": "首頁",
-		})
-	})
-
-	router.GET("/book/", b.Index)
+	router.StaticFS("/assets/static/",http.Dir("assets/static/"))
+	router.StaticFile("/favicon.ico", "assets/favicon.ico")
+	router.GET(projectPath+"/", index)
 	router.Run()
+}
+
+func index (c *gin.Context){
+	c.HTML(http.StatusOK, "index", gin.H{
+	})
 }
