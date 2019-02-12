7a054613859fbfbe592937e302ae883d60d392b6
RickyJian <rjricky03bc@gmail.com>
2019-02-12 16:38:30 +0800

    add database config

---
+++ database.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/spf13/viper"
+	"library/db"
+)
+
+func initDatabase(){
+	dbConfig := viper.GetStringMapString("database")
+	host := dbConfig["host"]
+	port := dbConfig["port"]
+	user := dbConfig["user"]
+	dbName := dbConfig["name"]
+	password := dbConfig["password"]
+	sslmode := dbConfig["sslmode"]
+	dbConn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",host,port,user,dbName,password,sslmode)
+	database, err := gorm.Open("postgres", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	database.SingularTable(true)
+	database.AutoMigrate(&db.Book{})
+	defer database.Close()
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/spf13/viper"
 )
 
@@ -16,8 +17,9 @@ func init() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	v := viper.GetStringMapString("project")
-	projectPath = v["name"]
+	p := viper.GetStringMapString("project")
+	projectPath = p["name"]
+	initDatabase()
 	initServer()
 }
 
