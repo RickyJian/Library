df7cd29aa300bebba0cac06452003377ec2d6dfa
ricky_jian <rjricky03bc@gmail.com>
2019-02-16 11:47:49 +0800

    add package init

--- main.go
+++ main.go
@@ -1,28 +1,10 @@
 package main
 
 import (
-	"fmt"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/spf13/viper"
-	"library/config"
+	_ "library/db"
+	_ "library/server"
 )
 
-var projectPath string
+func main(){
 
-func init() {
-	// init config
-	viper.SetConfigName("application")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("config/")
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println(err)
-	}
-	p := viper.GetStringMapString("project")
-	config.InitDatabase()
-	s := config.Server{Root:p["name"]}
-	s.Init()
-}
-
-func main() {
 }
